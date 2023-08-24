4ff94d9f2ecc8a609500a37f96d594a4c7ef3cef
anaseig <70536934+anaseig@users.noreply.github.com>
2023-08-24 14:42:10 +0600

    ui-updated

--- main.js
+++ main.js
@@ -1,78 +1,141 @@
 let playerPoint = 0;
 let computerPoint = 0;
-const rps = ["Rock", "Paper", "Scissors"];   //Array consisting of the choices
+let result = "";
+const rps = ["Rock", "Paper", "Scissors"];
 
 //Computer's Choice
-
 function getComputerChoice() {
-    return rps[Math.floor(Math.random() * 3)];   //Returns a random element of the rps array
+    return rps[Math.floor(Math.random() * 3)];
 }
 
-//Player's Choice
-
-function getPlayerChoice() {
-    let playerChoice = prompt("Select an option --> 1. Rock 2. Paper 3. Scissors: ");
-
-    if (playerChoice == 1) {
-        return "Rock";
-    } else if (playerChoice == 2) {
-        return "Paper";
-    } else if (playerChoice == 3) {
-        return "Scissors";
-    } else {
-        return alert("Enter a valid number");
-    }
+//Updates The Current Score
+function updatePlayerPoint() {
+    document.getElementById("playerPoints").textContent = playerPoint;
+}
+function updateCpuPoint() {
+    document.getElementById("cpuPoints").textContent = computerPoint;
 }
 
-//Game of Death
+//Game Status
+function gameStatus(result) {
+    document.getElementById("status").textContent = result;
+}
 
+function winner(result) {
+    document.getElementById("winner").textContent = result;
+}
+
+function previousResult(result) {
+    document.getElementById("previousResult").textContent = result;
+    let colored = document.getElementById("previousResult");
+    colored.style.color = "red";
+}
+
+
+//Game Logic
 function playRound(playerSelection, computerSelection) {
-    console.log("You selected: " + playerSelection);
-    console.log("Computer selected: " + computerSelection);
 
+    //Player wins
     if (playerSelection === rps[0] && computerSelection === rps[2]) {
-        playerPoint = playerPoint + 1;
-        return "You won!";
+        playerPoint++;
+        gameStatus("AI selected: Scissors. You won!");
+        updatePlayerPoint();
     } else if (playerSelection === rps[1] && computerSelection === rps[0]) {
-        playerPoint = playerPoint + 1;
-        return "You won!";
+        playerPoint++;
+        gameStatus("AI selected: Rock. You won!");
+        updatePlayerPoint();
     } else if (playerSelection === rps[2] && computerSelection === rps[1]) {
-        playerPoint = playerPoint + 1;
-        return "You won!";
+        playerPoint++;
+        gameStatus("AI selected: Paper. You won!");
+        updatePlayerPoint();
+
+    //Draw
     } else if (playerSelection === rps[0] && computerSelection === rps[0]) {
-        return "Draw";
-    }   else if (playerSelection === rps[1] && computerSelection === rps[1]) {
-        return "Draw";
-    }   else if (playerSelection === rps[2] && computerSelection === rps[2]) {
-        return "Draw";
+        playerPoint += 0.5;
+        computerPoint += 0.5;
+        gameStatus("AI also selected: Rock. It's a draw.");
+        updatePlayerPoint();
+        updateCpuPoint();
+    } else if (playerSelection === rps[1] && computerSelection === rps[1]) {
+        playerPoint += 0.5;
+        computerPoint += 0.5;
+        gameStatus("AI also selected: Paper. It's a draw.");
+        updatePlayerPoint();
+        updateCpuPoint();
+    } else if (playerSelection === rps[2] && computerSelection === rps[2]) {
+        playerPoint += 0.5;
+        computerPoint += 0.5; 
+        gameStatus("AI also selected: Scissors. It's a draw.");
+        updatePlayerPoint();
+        updateCpuPoint();
+
+    //CPU wins
+    } else if (playerSelection === rps[2] && computerSelection === rps[0]) {
+        computerPoint++;
+        document.getElementById("cpuPoints").textContent = computerPoint;
+        gameStatus("AI selected: Rock. CPU won!");
+    } else if (playerSelection === rps[0] && computerSelection === rps[1]) {
+        computerPoint++;
+        document.getElementById("cpuPoints").textContent = computerPoint;
+        gameStatus("AI selected: Paper. CPU won!");
+    } else if (playerSelection === rps[1] && computerSelection === rps[2]) {
+        computerPoint++;
+        document.getElementById("cpuPoints").textContent = computerPoint;
+        gameStatus("AI selected: Scissors. CPU won!");
+
     } else {
-        computerPoint = computerPoint + 1;
-        return "You lose";
+        gameStatus("ERROR");
+    }
+
+    //Result after 5 rounds
+    if (playerPoint >= 5) {
+        winner(`Congratulations! You've won!!`);
+        previousResult(`result: ${playerPoint} - ${computerPoint}`);
+        playerPoint = 0;
+        computerPoint = 0;
+        updatePlayerPoint();
+        updateCpuPoint();
+    } else if (computerPoint >= 5) {
+        winner("the AI has won. Try again?");
+        previousResult(`result: ${playerPoint} - ${computerPoint}`);
+        playerPoint = 0;
+        computerPoint = 0;
+        updatePlayerPoint();
+        updateCpuPoint();
+    } else if (playerPoint >= 5 && computerPoint >= 5) {
+        playerPoint = 0;
+        computerPoint = 0;
+        updatePlayerPoint();
+        updateCpuPoint();
+        winner("The round is a draw");
+        previousResult(`result: ${playerPoint} - ${computerPoint}`);
+    }
+
+    if (playerPoint >= 0.5 || computerPoint >= 0.5) {
+        winner("");
+        previousResult("");
     }
 }
 
-// 5 Round Game
+//USER INTERFACE
+let btn1 = document.getElementById('rock');
+let btn2 = document.getElementById('paper');
+let btn3 = document.getElementById('scissors');
 
-function play() {
-    let count = 0;
+btn1.addEventListener("click", function(e) {
+    let playerSelection = rps[0];
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+});
 
-    while (count < 5) {
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
+btn2.addEventListener("click", function(e) {
+    let playerSelection = rps[1];
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+});
 
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("scoreboard: " + playerPoint + "-" + computerPoint); 
-
-        count++;
-    }
-
-    if (playerPoint > computerPoint) {
-        console.log("Player won the tournament!");
-    } else if (playerPoint === computerPoint) {
-        console.log("The tournament is draw!");
-    } else {
-        console.log("Computer won the tournament!");
-    }
-}
-
-play();
+btn3.addEventListener("click", function(e) {
+    let playerSelection = rps[2];
+    let computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+});
